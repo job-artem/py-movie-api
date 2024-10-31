cea709a91198185fd116df54d36323680d710a7e
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-10-31 12:58:52 +0200

    POST api/cinema/movies/ - should create a new movie based on passed data

--- cinema/views.py
+++ cinema/views.py
@@ -1,21 +1,31 @@
-from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return JsonResponse(serializer.data, safe=False, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+@api_view(['GET'])
 def movie_detail(request, pk):
     if request.method == "GET":
         movie = get_object_or_404(Movie, pk=pk)
         serializer = MovieSerializer(movie)
-        return JsonResponse(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
