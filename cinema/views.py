369c0e013d848bd5eb324079449db9809a7b0315
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-10-31 14:55:57 +0200

    ready solution!

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,7 +8,7 @@ from cinema.serializers import MovieSerializer
 
 
 # Create your views here.
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
@@ -21,9 +20,13 @@ def movie_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-@api_view(['GET', 'PUT', 'DELETE'])
+
+@api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
